b0a8c5fb818bba0e040a138fc8093031d401121a
Nurlanbek Duishoev <nurlanbek.duishoev@gmail.com>
2024-09-24 14:52:29 +0200

    added requirements.txt for development

--- einmind/__init__.py
+++ einmind/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'EinMind Inc.'
-__version__ = '1.0'
+__version__ = '0.1.0'
 
 from einmind.icd10cm.icd10cm_client import ICD10CMClient
 from einmind.snomed_ct.snomed_ct_client import SNOMEDCTClient
